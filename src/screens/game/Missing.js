6079b1c50e134ab2193ac5c9d47c0e23ba0db63f
nghochi123 <nghochi123@gmail.com>
2022-02-17 14:02:41 +0800

    missing page

--- src/screens/game/Missing.js
+++ src/screens/game/Missing.js
@@ -1,14 +1,37 @@
 // Fallback page
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Tutorial7(props) {
+  let navigate = useNavigate();
   return (
-    <div style={{ margin: 20 }}>
-      <p>
-        Hi, this page doesnt exist. Click{" "}
-        <a href="https://settlersofyang.netlify.app/tutorial/welcome">here </a>
-        to go to where you started.
-      </p>
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        textAlign: "center",
+      }}
+    >
+      <p>Oh no, are you lost?</p>
+      <p>Let me bring you back to where you came from.</p>
+      <button
+        style={{
+          backgroundColor: "#4CAF50" /* Green */,
+          border: "none",
+          color: "white",
+          padding: "15px 32px",
+          textAlign: "center",
+          textDecoration: "none",
+          display: "inline-block",
+          fontSize: "16px",
+          cursor: "pointer",
+        }}
+        onClick={() => navigate(-1)}
+      >
+        Go back
+      </button>
     </div>
   );
 }
