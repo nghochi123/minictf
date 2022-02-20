83246c4bf67ebf8d391686389e158e0226dd3ca6
MingEn82 <mingen8216@gmail.com>
2022-02-20 16:19:14 +0800

    updated map

--- src/screens/game/createjsfiles.py
+++ src/screens/game/createjsfiles.py
@@ -63,9 +63,8 @@ export default function TutorialMain() {{
       <div
         style={{{{
           position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          top: "0%",
+          left: "0%",
           textAlign: "center",
         }}}}
       >
